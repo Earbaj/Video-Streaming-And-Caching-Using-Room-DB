889dae0cfe4454c21574daf0c049267e85ff0c4b
earba <earbajsaria3@gmail.com>
2023-09-22 12:47:17 +0600

    add empty packages and create details fragment and navigation fragment and linking with main activity xml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
     // Glide for images
     implementation("com.github.bumptech.glide:glide:4.8.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
