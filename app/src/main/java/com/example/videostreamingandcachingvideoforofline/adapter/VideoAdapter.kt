4a9cd8fff17c9910c1f4bd2815ec13e996098755
earba <earbajsaria3@gmail.com>
2023-09-23 12:49:17 +0600

    Create Video adapter for recycler view and binding adapter for loading image with glide and link to details fragment

---
+++ app/src/main/java/com/example/videostreamingandcachingvideoforofline/adapter/VideoAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.videostreamingandcachingvideoforofline.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.videostreamingandcachingvideoforofline.R
+import com.example.videostreamingandcachingvideoforofline.databinding.ListItemBinding
+import com.example.videostreamingandcachingvideoforofline.model.Video
+
+class VideoAdapter : RecyclerView.Adapter<VideoViewHolder>(){
+
+
+    /**
+     * The videos that our Adapter will show
+     */
+    var videos: List<Video> = emptyList()
+        set(value) {
+            field = value
+            // For an extra challenge, update this to use the paging library.
+
+            // Notify any registered observers that the data set has changed. This will cause every
+            // element in our RecyclerView to be invalidated.
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
+        val withDataBinding: ListItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            VideoViewHolder.LAYOUT,
+            parent,
+            false)
+        return VideoViewHolder(withDataBinding)
+    }
+
+    override fun getItemCount(): Int {
+        return videos.size
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position. This method should
+     * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
+     * position.
+     */
+    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+        holder.viewDataBinding.also {
+            it.video = videos[position]
+        }
+    }
+}
+
+
+class VideoViewHolder(val viewDataBinding: ListItemBinding): RecyclerView.ViewHolder(viewDataBinding.root){
+    companion object{
+        @LayoutRes
+        val LAYOUT = R.layout.list_item
+    }
+}
--- app/src/main/java/com/example/videostreamingandcachingvideoforofline/ui/DetailsFragment.kt
+++ app/src/main/java/com/example/videostreamingandcachingvideoforofline/ui/DetailsFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.videostreamingandcachingvideoforofline.R
+import com.example.videostreamingandcachingvideoforofline.adapter.VideoAdapter
 import com.example.videostreamingandcachingvideoforofline.databinding.FragmentDetailsBinding
+import com.example.videostreamingandcachingvideoforofline.model.Video
 import com.example.videostreamingandcachingvideoforofline.viewmodel.DetailsViewModel
 import com.example.videostreamingandcachingvideoforofline.viewmodel.Factory
 
@@ -20,9 +25,22 @@ class DetailsFragment : Fragment() {
         ViewModelProvider(this, Factory(activity.application)).get(DetailsViewModel::class.java)
     }
 
+    /**
+     * RecyclerView Adapter for converting a list of Video to cards.
+     */
+    private var videoAdapter: VideoAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.playlist.observe(viewLifecycleOwner, Observer<List<Video>>{
+            it.apply {
+                videoAdapter?.videos = it
+            }
+        })
     }
 
     override fun onCreateView(
@@ -33,6 +51,11 @@ class DetailsFragment : Fragment() {
         val bindings = FragmentDetailsBinding.inflate(layoutInflater)
         bindings.lifecycleOwner = this
         bindings.viewModel = viewModel
+        videoAdapter = VideoAdapter()
+        bindings.root.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = videoAdapter
+        }
         return bindings.root
     }
 
