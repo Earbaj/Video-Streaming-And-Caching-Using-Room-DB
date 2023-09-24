43bb5a9057a99ac860bd1dab35eb845b6ced6963
earba <earbajsaria3@gmail.com>
2023-09-24 17:34:41 +0600

    Create utils class and add short description methode

--- app/src/main/java/com/example/videostreamingandcachingvideoforofline/model/Video.kt
+++ app/src/main/java/com/example/videostreamingandcachingvideoforofline/model/Video.kt
@@ -1,9 +1,14 @@
 package com.example.videostreamingandcachingvideoforofline.model
 
+import com.example.videostreamingandcachingvideoforofline.utils.smartTruncate
+
 data class Video(
     val title: String,
     val description: String,
     val url: String,
     val updated: String,
     val thumbnail: String
-)
+){
+    val shortDescription: String
+        get() = description.smartTruncate(200)
+}
